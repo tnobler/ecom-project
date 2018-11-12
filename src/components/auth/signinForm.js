5a807be00343d0c990f231c145f33f16e6ffaa2f
Tanner Noble <tannernoble@gmail.com>
2018-11-12 16:08:25 -0600

    styled details quick links component

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -48,7 +48,7 @@ class SignInForm extends Component {
                 title='Login'
                 name='login'
                 component={FormButton}/>
-                <Details className='sign-in-form__details' title='QuickLinks' links={links}/>
+            <Details className='sign-in-form__details' title='QuickLinks' links={links}/>
       </form>
     );
   }
