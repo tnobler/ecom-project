2c8639493774a0616ee1b8e964abac47ca1682df
Tanner Noble <tannernoble@gmail.com>
2018-11-12 22:24:30 -0600

    built base structure for account components

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -5,24 +5,8 @@ import {
 
 
 const INITIAL_STATE = {
-    headerLinks: [
-        {
-            title: 'yooo'
-        },
-        {
-            title: 'YOOOO'
-        }
-    ],
-    navbarLinks: [
-        {
-            title: 'account',
-            active: true
-        },
-        {
-            title: 'purchases',
-            active: false
-        }
-    ]
+    headerLinks: [],
+    navbarLinks: []
 }
 
 export default function(state = INITIAL_STATE, action) {
