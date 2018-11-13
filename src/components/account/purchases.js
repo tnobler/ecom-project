b203567eb93af4fd03e5824f621e8da043805cd2
Tanner Noble <tannernoble@gmail.com>
2018-11-13 17:29:35 -0600

    finished purchases component

--- src/components/account/purchases.js
+++ src/components/account/purchases.js
@@ -12,10 +12,14 @@ class Purchases extends Component {
   render () {
       const { className } = this.props;
     return (
-      <div className={`${className} puchases`}>
+      <div className={`${className} purchases`}>
         {
             this.props.purchases.map(purchase => {
-                return <div>{purchase.title}</div>
+                return (
+                    <div key={purchase._id} className='purchases__purchase purchase'>
+                        <img className='purchase__img' src='http://via.placeholder.com/80x80'/>
+                    </div>
+                    )
             })
         }
       </div>
