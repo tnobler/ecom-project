c9bc7c3903de9d47ef2f01cdd17dbc9ade903e3f
Tanner Noble <tannernoble@gmail.com>
2018-11-15 13:40:04 -0600

    built base for shop component, built shop route etc

---
+++ src/components/shop/shop.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Shop extends Component {
+
+    componentDidMount() {
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Login',
+                path: '/signin'
+            }
+        ]
+        this.props.setHeaderLinks(headerLinks);
+        // fetch shop products action creator
+        // set the header links
+        // fetch navbar links
+            // set the navbar links
+            // filter products with links
+    }
+    render () {
+        return (
+            <div className='shop'>
+                {/* shop searchBar Component */}
+                {/* shop product Component */}
+                {/* shop cart Component */}
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return { state }
+}
+
+Shop = connect(mapStateToProps, actions)(Shop);
+
+export default Shop
+
+
+
