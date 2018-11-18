ba20523179afced7288899d4eba58cd8a35fe913
Tanner Noble <tannernoble@gmail.com>
2018-11-18 17:28:34 -0600

    built the information routes and container components payment and shipping js

---
+++ src/components/information/payment.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import PageTitle from '../pageTitle';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Payment extends Component {
+
+  componentDidMount() {
+    this.props.setHeaderLinks([]);
+    this.props.setNavbarLinks([]);
+  }
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
+  render () {
+    return (
+      <div className='payment'>
+        <PageTitle className='payment__page-title' title='Payment Information' />
+        {/* <paymentForm onSubmit={this.onSubmit} className='payment__form' /> */}
+      </div>
+    );
+  }
+}
+
+Payment = connect(null, actions)(Payment);
+
+export default Payment;
---
+++ src/components/information/shipping.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import PageTitle from '../pageTitle';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Shipping extends Component {
+
+  componentDidMount() {
+    this.props.setHeaderLinks([]);
+    this.props.setNavbarLinks([]);
+  }
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
+  render () {
+    return (
+      <div className='shipping'>
+        <PageTitle className='shipping__page-title' title='Shipping Address' />
+        {/* <ShippingForm onSubmit={this.onSubmit} className='shipping__form' /> */}
+      </div>
+    );
+  }
+}
+
+Shipping = connect(null, actions)(Shipping);
+
+export default Shipping;
