9e0ee69613b8f53560cf46c286926d315ff46338
Tanner Noble <tannernoble@gmail.com>
2018-11-17 22:35:19 -0600

    built review form buttons

--- src/components/order/review.js
+++ src/components/order/review.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+
 import PageTitle from '../pageTitle';
+import ReviewForm from './reviewForm';
+
 
 
 
@@ -12,10 +15,16 @@ class Review extends Component {
         this.props.setHeaderLinks([]);
         this.props.setNavbarLinks([]);
     }
+
+    onSubmit = (fields) => {
+        console.log('fields');
+    }
+
     render () {
         return (
             <div className='review'>
                 <PageTitle className='review__page-title' title='Order Review'/>
+                <ReviewForm className='review__form' onSubmit={this.onSubmit}/>
             </div>
         );
     }
